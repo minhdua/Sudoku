1eddb62d44cc3746094b1a3ccda938a782bec774
Võ Thanh Hiền <43074707+minhdua@users.noreply.github.com>
2018-11-15 11:55:43 +0700

    version 2
    Cải tiến mảng đánh dấu

--- sudoku.cpp
+++ sudoku.cpp
@@ -2,95 +2,74 @@
 #include "console.cpp"
 #define FIXED 4
 #define DEFAULT 7
-#define LEVEL 0
-/*
-  * tao mot bang Sudoku 9x9 voi cac gia tri la 0
-  * mau mat dinh cua moi o la mau trang 15
-  * level mat dinh la de = 35
-  * dat ta ca cac bit trong hang,cot,lo la 0
+#define LEVEL 20
+/* Phuong thuc xay dung mac nhien
+  * Khoi tao cac gia tri la 0
+  * Khoi tao cac mang danh dau la 0
+  * Thiet lap level mat dinh
 */
 Sudoku::Sudoku(){
-	Clear();
+	for(int i=0;i<9;i++){
+		for(int j=0;j<9;j++){
+			s[i][j].num=0;
+			s[i][j].color=DEFAULT;
+		}
+	}
+	for(int i=1;i<=9;i++){
+		mark[i]=0;
+	}
+	level=LEVEL;
 }
-/*
+/* PHUONG THUC XAY DUNG SAO CHEP
 	* sao chep so va mau tu bang s
   	* sao chep level
-  	* sao chep tap ca gia tri cua cac nhom
+  	* sao chep tat ca gia tri cua cac nhom
 */
 Sudoku::Sudoku(Sudoku &other){
 	for(int i=0;i<9;i++){
 		for(int j=0;j<9;j++){
-			SetNum(i,j,other.GetNum(i,j));
-			SetColor(i,j,other.GetColor(i,j));
+			s[i][j].num=other.GetNum(i,j);
+			s[i][j].color=other.GetColor(i,j);
 		}
 	}
-	level=other.level;
-	for(int i=0;i<9;i++){
-		SetMarkRow(i,other.GetMarkRow(i));
-		SetMarkRow(i,other.GetMarkCol(i));
-		SetMarkRow(i,other.GetMarkBlock(i));
-	}
+	for(int i=1;i<=9;i++) mark[i]=other.GetMark(i);
+	level=other.GetLevel();//dat level tuong ung
 }
-/*
-  * tao mot bang Sudoku 9x9 voi cac gia tri la 0
-  * mau mat dinh cua moi o la mau trang 15
-  * level mat dinh la de = 35
-  * dat ta ca cac bit trong hang,cot,lo la 0
+/* DAT GIA TRI
+	*Dat gia tri z vao o x y
+	*Bat cac bit o vi tri z tu cac hang, cot va cac lo
 */
-void Sudoku::Clear(){
-	for(int i=0;i<9;i++){
-		for(int j=0;j<9;j++){
-			SetNum(i,j,0);
-			SetColor(i,j,DEFAULT);
-		}
-	}
-	SetLevel(LEVEL);
-	for(int i=0;i<9;i++){
-		SetMarkRow(i,0);
-		SetMarkCol(i,0);
-		SetMarkBlock(i,0);
-	}
+int Sudoku::SetNum(int x,int y,int z) {
+	s[x][y].num=z;
+	OnBit(x,y,z);
 }
-/*
-	*kiem tra gia tri z trong hang x, cot y va lo x/3*y+y/3
-	*tra ve true neu gia tri z chua co trong hang x,cot y, lo x/3*3+y/3
-	*tra ve false trong truong hop nguoc lai
-*/
-bool Sudoku::Check(int x,int y,int z){
-	return (!CheckRow(x,z) && !CheckCol(y,z) && !CheckBlock(x,y,z));
-}
-/*
-	*Danh dau gia tri z da co trong hang x,cot y, lo x/3*3+y
-*/
-void Sudoku::OnBit(int x,int y,int z){
-	OnBitRow(x,z);
-	OnBitCol(y,z);
-	OnBitBlock(x,y,z);
-}
-/*
-	*Danh dau gia tri z da co trong hang x,cot y, lo x/3*3+y
-*/
-void Sudoku::OffBit(int x,int y,int z){
-	OffBitRow(x,z);
-	OffBitCol(y,z);
-	OffBitBlock(x,y,z);
-}
-/*
+/* XOA GIA TRI
 	*Xoa gia tri trong o x y
 	*Tat cac bit co gia tri tai o x,y
 */
 void Sudoku::Erase(int x,int y){
-	int z=GetNum(x,y);
-	SetNum(x,y,0);
-	OffBit(x,y,z);
+	int z=s[x][y].num;
+	if (z!=0){
+		OffBit(x,y,z);
+		s[x][y].num=0;
+		s[x][y].color=DEFAULT;
+	}
 }
-/*
+/* LAM SACH SUDOKU
+  * Xoa cac gia tri trong o x,y
+  * Dat gia tri mau mat dinh
+*/
+void Sudoku::Clear(){
+	for(int i=0;i<9;i++)
+		for(int j=0;j<9;j++) Erase(i,j);
+}
+/* SINH GIA TRI NGAU NHIEN
 	*sinh ngau nhien hang x, cot y voi gia tri z
 	*kiem tra gia tri z khi them vao o x,y
 	*danh dau hang, cot, lo voi gia tri z
 	*thay doi mau trang sang mau do
 */
-void Sudoku::GenerateBoard(){
+void Sudoku::Generate(){
 	srand(time(0));
 	int x,y,z;
 	int i=0;
@@ -99,16 +78,18 @@ void Sudoku::GenerateBoard(){
 		y=rand()%9;		//y la chi so cot co gia tri tu 0 den 8
 		z=1+rand()%9;	//z la so trong o gia tri tu 1 den 9
 		//neu o trong va vi tri an toan thi them vao
-		cout<<x<<" "<<y<<" "<<z<<endl;
-		if(GetNum(x,y)==0&&Check(x,y,z)){
-			SetNum(x,y,z);
-			SetColor(x,y,FIXED);
-			OnBit(x,y,z);
+		if(GetNum(x,y)==0&&Check(x,y,z)){ //neu o chua co gia tri va gia tri z la hop le
+			SetNum(x,y,z);//dat gia tri z
+			SetColor(x,y,FIXED); // dat mau co dinh
 			i++;
 		}
 	}
 }
-/*
+void Sudoku::NewDisplay(){
+	clrscr();
+	Display();
+}
+/* HIEN THI SUDOKU
 	*nhap -1 de thoat
  	*Nhap toa do x,y,z
  	*Thiet lap rang buoc cho gia tri nhap vao, toa do 0-8, gia tri 1-9
@@ -119,13 +100,10 @@ void Sudoku::GenerateBoard(){
  	*Neu z = 0 thi xoa o do
  	
 */
-void Sudoku::NewDisplay(){
-	clrscr();
-	DisplaySudoku();
-}
+
 void Sudoku::PlayerSolveSudoku(){
 	int x,y,z;
-	DisplaySudoku();
+	Display();
 	do{
 		cout<<"nhap toa do x y va z , nhap  -1 de thoat !"<<endl;
 		cout<<"lua chon cua ban: ";
@@ -143,7 +121,6 @@ void Sudoku::PlayerSolveSudoku(){
 					{
 						Erase(x,y);
 						SetNum(x,y,z);
-						OnBit(x,y,z);
 						NewDisplay();
 					}else cout<<"Gia tri da ton tai! "<<endl;//Kiem tra tinh hop le cua so nhap vao
 				}else cout<<"Vi tri khong cho phep! "<<endl;//Kiem tra o nhap co hop le khong
@@ -170,7 +147,6 @@ bool Sudoku::ComputerSolveSudoku(int x,int y){
 	for(int z=1;z<=9;z++){
 		if(Check(x,y,z)){
 			SetNum(x,y,z);
-			OnBit(x,y,z);
 			if(ComputerSolveSudoku(x,y)) return true;
 			Erase(x,y);
 		}
@@ -182,88 +158,44 @@ bool Sudoku::ComputerSolveSudoku(int x,int y){
   *Hien thi cot chi muc
   *Hien thi vung 3x3
 */
-void DisplayIndexCol(){
-	for(int i=0;i<=9;i++){
-		if(i%3==0){
-			if(i==0) cout<<"   ";
-			else cout<<" "<<i-1<<"  ";
-		}
-		else cout<<" "<<i-1;
-	}
-	cout<<endl;
-}
-void DisplayLineBlock(){
-	for(int i=0;i<=9;i++){
-		if(i%3==0){
-			if(i==0) cout<<"  +";
-			else cout<<"---"<<"+";
-		}
-		else cout<<"--";
-	}
-	cout<<endl;
-}
-void Sudoku::DisplayRowSudoku(int i){
-	for(int j=0;j<=9;j++){
-		if (j%3==0){
-			if(j==0) cout<<i-1<<" |";
-			else {
-				TextColor(GetColor(i-1,j-1));
-				if(GetNum(i-1,j-1)==0) cout<<" . ";
-				else cout<<" "<<GetNum(i-1,j-1)<<" ";
-				TextColor(DEFAULT);
-				cout<<"|";
-			}
-		}else {
-			TextColor(GetColor(i-1,j-1));
-			if(GetNum(i-1,j-1)==0) cout<<" .";
-				else cout<<" "<<GetNum(i-1,j-1);
-			TextColor(DEFAULT);
-		}
+void Sudoku::RawRowSudoku(int i){
+	cout<<i-1<<" |";
+	for(int j=1;j<=9;j++){
+		TextColor(GetColor(i-1,j-1));
+		if(GetNum(i-1,j-1)==0) cout<<" .";
+			else cout<<" "<<GetNum(i-1,j-1);
+		TextColor(DEFAULT);
+		if (j%3==0) cout<<" |";	
 	}
 	cout<<endl;
 }
 /*
-	
-}*/
-void Sudoku::DisplaySudoku(){	
+	*ve 	
+*/
+void Sudoku::Display(){
+	string lineindex="    0 1 2   3 4 5   6 7 8\n";
+	string lineblock="  +-------+-------+-------+\n";	
 	for(int i=0;i<=9;i++){
 		if(i%3==0){
 			if (i==0){
-				DisplayIndexCol();
-				DisplayLineBlock();
+				cout<<lineindex;
+				cout<<lineblock;
 			}
 			else {
-				DisplayRowSudoku(i);
-				DisplayLineBlock();
+				RawRowSudoku(i);
+				cout<<lineblock;
 			}
-		}else DisplayRowSudoku(i);
+		}else RawRowSudoku(i);
 	}
 }
-/*
-
-*/
 int main(){
 	Sudoku s;
-	s.GenerateBoard();
-	for(int i=0;i<9;i++){
-		for(int j=0;j<9;j++){
-			if(s.GetNum(i,j)==0) cout<<". ";
-			else
-			cout<< s.GetNum(i,j)<<" ";
-		}
-		cout<<endl;
-	}
-	cout<<"=============mau: ============"<<endl;
-	for(int i=0;i<9;i++){
-		for(int j=0;j<9;j++){
-			cout<< s.GetColor(i,j)<<" ";
-		}
-		cout<<endl;
-	}
-	for(int i=0;i<9;i++)
-	cout<<s.markblock[i]<<endl;	
-	//s.PlayerSolveSudoku();
-	s.ComputerSolveSudoku(0,0);
-	s.NewDisplay();
+	s.Generate();
+	s.Display();
+	Sudoku s1(s);
+	s1.PlayerSolveSudoku();
+	Sudoku s2(s);
+	if (s2.ComputerSolveSudoku(0,0)) s2.Display();
+	else cout<<"Khong tim ra giai phap !";
 }
 
--- sudoku.h
+++ sudoku.h
@@ -2,6 +2,7 @@
 #ifndef  SUDO_H
 #define SUDO_H
 #include <bits/stdc++.h>
+#define MASH(x,y) ((1<<x)+(1<<(y+9))+(1<<(x/3*3+y/3+18)))
 using namespace std;
 typedef struct Cell {
 	int num;
@@ -12,46 +13,28 @@ class Sudoku {
 		Sudoku();//xay dung mac nhien
 		Sudoku(Sudoku &s);//xay dung sao chep
 		~Sudoku(){};//phuong thuc huy
-		int GetLevel() {return level;}//lay level
-		int GetNum(int x,int y) {return s[x][y].num;}//lay gia tri tai o x,y
-		int SetNum(int x,int y,int z) {s[x][y].num=z;}//Dat gia tri tai o x,y
+		int GetLevel(){return level;}//lay level
 		int SetLevel(int newlevel) {level=newlevel;}//Dat level
+		int GetNum(int x,int y) {return s[x][y].num;}//lay gia tri tai o x,y
+		int SetNum(int x,int y,int z);//Dat gia tri tai o x,y
 		int GetColor(int x, int y){return s[x][y].color;}//Lay mau tai o x,y
 		int SetColor(int x,int y,int c){s[x][y].color=c;}//dat may tai o x,y
+		int GetMark(int i){ return mark[i];} // lay phan thu danh dau thu i
 		void Erase(int x,int y);// xoa gia tri trong bang tai o x,y
 		void Clear();//xoa tat ca cac phan tu trong o
-		void SetMarkRow(int x,int z){markrow[x]=z;}//Dat gia tri cho hang x
-		void SetMarkCol(int y,int z){markcol[y]=z;}//Dat gia tri cho cot y
-		void SetMarkBlock(int t,int z){markblock[t]=z;}//Dat gia tri cho lo t
-		int GetMarkRow(int x){return markrow[x];}//Lay gia tri cho hang x
-		int GetMarkCol(int y){return markcol[y];}//Lay gia tri cho cot y
-		int GetMarkBlock(int t){return markblock[t];}//Lay gia tri cho lo t
-		bool CheckRow(int x,int z) { return (markrow[x]&(1<<(z-1)));}//kiem tra hang
-		bool CheckCol(int y,int z) { return (markcol[y]&(1<<(z-1)));}//kiem tra cot
-		bool CheckBlock(int x,int y,int z){ return (markblock[x/3*3+y/3]&(1<<(z-1)));}//kiem tra lo
-		bool Check(int x,int y,int z);//kiem tra hang x da co chua gia tri z hay chua
-		void OnBitRow(int x,int z){(markrow[x]|=(1<<(z-1)));}//Bat bit thu z cua hang x
-		void OnBitCol(int y,int z){(markcol[y]|=(1<<(z-1)));}//Bat bit thu z cua cot y
-		void OnBitBlock(int x,int y,int z){(markblock[x/3*3+y/3]|=(1<<(z-1)));}//Bat bit thu z cua lo x/3*3+y
-		void OffBitRow(int x,int z){markrow[x]&= (~(1<<(z-1)));}//tat bit thu z cua hang x
-		void OffBitCol(int y,int z){markcol[y]&= (~(1<<(z-1)));}//tat bit thu z cua cot y
-		void OffBitBlock(int x,int y,int z){markblock[x/3*3+y/3]&= (~(1<<(z-1)));}//tat bit thu z cua lo x/3*3+y/3
-		void OnBit(int x,int y,int z);//danh dau gia tri z da co tai o x,y
-		void OffBit(int x,int y,int z);//danh dau gia tri z da co tai o x,y
-		void GenerateBoard();//Sinh ngay nhien mot bang sudoku
+		bool Check(int x,int y,int z) {return ((mark[z]& (~MASH(x,y)))==mark[z]);}//z co the them vao hang x cot y hay khong
+		void OnBit(int x,int y,int z) {mark[z]|= MASH(x,y);}//Bat gia tri z 
+		void OffBit(int x,int y,int z){mark[z] &= ~MASH(x,y);};//Tat gia tri z 
+		void Generate();//Sinh ngay nhien mot bang sudoku
 		void PlayerSolveSudoku();//Nguoi choi giai quyet Sudoku
 		bool ComputerSolveSudoku(int x,int y);//May giai quyet sudoku
-		void NewDisplay();
-		void DisplayRowSudoku(int i);
-		void DisplaySudoku();//Hien thi sudoku
+		void RawRowSudoku(int i);
+		void Display();//Hien thi sudoku
 		bool NextBlank(int &x,int &y);
-		const int* operator [] (unsigned int i) const;//qua tai toan tu
-		int* operator [] (unsigned int i);//qua tai toan tu
+		void NewDisplay();
 	public:
 		Cell s[9][9];//bang sudoku
-		int markrow[9];//danh dau 1 so da co trong hang
-		int markcol[9];//danh dau 1 so da co trong cot
-		int markblock[9];//danh dau 1 so da cos trong lo
+		int mark[10];
 		int level;// do kho cua sudoku
 };
 #endif // SUDO_H
